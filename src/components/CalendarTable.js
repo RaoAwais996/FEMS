58acbe9adec438219fb0fe7f4ba0fc3b1501ccff
Rao Awais <ra848683@gmail.com>
2023-06-25 19:43:01 +0500

    Table changes, Language switch, Db bug, calendar tangle solve

--- src/components/CalendarTable.js
+++ src/components/CalendarTable.js
@@ -6,6 +6,7 @@ import EmployeesTable from './EmployeesManagement';
 import { useNavigate } from 'react-router-dom';
 import { db } from '../firebase';
 import CustomDropdown from './CustomDropdown';
+import { useRef } from 'react';
 
 const CalendarTable = ({ userType,useremail }) => {
   const [currentDate, setCurrentDate] = useState(moment());
@@ -13,6 +14,9 @@ const CalendarTable = ({ userType,useremail }) => {
   const [employees, setEmployees] = useState([]);
   const [showManageEmployees, setShowManageEmployees] = useState(false);
   const [employeeName, setEmployeeName] = useState('');
+  const [isHeaderSticky, setIsHeaderSticky] = useState(false);
+  const tableHeadRef = useRef(null);
+
 
   const navigate = useNavigate(); 
 
@@ -48,160 +52,222 @@ const CalendarTable = ({ userType,useremail }) => {
       }
     };
 
+    
+
     fetchEmployees();
   }, [userType, useremail]);
 
-
-
-
-  const renderCalendarBody = () => {
-  const daysInMonth = currentDate.daysInMonth();
-  const firstDayOfMonth = currentDate.clone().startOf('month');
-  // const firstDayOfWeek = firstDayOfMonth.weekday();
-  const today = moment();
-  const calendarRows = [];
-
-
-
-  const handleCellChange = (employeeIndex, date, value) => {
-    const updatedEmployees = [...employees];
-    const selectedEmployee = updatedEmployees[employeeIndex];
+  useEffect(() => {
+      const handleScroll = () => {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const threshold = 100;
   
-    // Only allow editing if the user is an employee and the email matches
-    if (userType === 'hr') {
-      let updatedCount = 0; // Variable to track the number of updated employees
+        setIsHeaderSticky(scrollTop > threshold);
+      };
   
-      if (value === 'Day off' && userType === 'hr') {
-        updatedEmployees.forEach((employee) => {
-          if (!employee.fields) {
-            employee.fields = {};
-          }
-          if (employee.fields[date.format('YYYY-MM-DD')] !== value) {
-            employee.fields[date.format('YYYY-MM-DD')] = value;
-            updatedCount++;
-          }
-        });
-      
-        setEmployees(updatedEmployees);
-      
-        if (updatedCount > 0) {
-          // alert(`Field value updated for ${updatedCount} employee(s) successfully.`);
-        }
-      
-        // Update the Firestore database for all employees
-        const batch = db.batch();
-        updatedEmployees.forEach((employee) => {
-          const employeeId = employee.id;
-          const employeeRef = db.collection('employees').doc(employeeId);
-          batch.set(
-            employeeRef,
-            {
-              fields: {
-                [date.format('YYYY-MM-DD')]: employee.fields[date.format('YYYY-MM-DD')],
-              },
-            },
-            { merge: true }
-          );
-        });
-      
-        batch
-          .commit()
-          .then(() => {
-            alert('Field values updated in Firebase successfully.');
-          })
-          .catch((error) => {
-            alert('Error updating field value in Firebase:', error);
-          });
-                      // alert('Field value updated in Firebase successfully.');
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
 
-      }
-       else {
-        // Set the value for the selected employee's field
-        if (!selectedEmployee.fields) {
-          selectedEmployee.fields = {};
+    useEffect(() => {
+      const tableHeadElement = tableHeadRef.current;
+      const tableHeadCells = tableHeadElement.querySelectorAll('th');
+  
+      const handleResize = () => {
+        tableHeadCells.forEach((cell, index) => {
+          const originalCell = document.querySelector(`.calendar-table thead th:nth-child(${index + 1})`);
+          cell.style.width = originalCell.offsetWidth + 'px';
+        });
+      };
+  
+      handleResize();
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+  
+
+const handleCellChange = (employeeIndex, date, value) => {
+  const updatedEmployees = [...employees];
+  const selectedEmployee = updatedEmployees[employeeIndex];
+
+  // Only allow editing if the user is an HR and the email matches
+  if (userType === 'hr') {
+    let updatedCount = 0; // Variable to track the number of updated employees
+
+    if (value === 'Ditë e lirë' && userType === 'hr') {
+      updatedEmployees.forEach((employee) => {
+        if (!employee.fields) {
+          employee.fields = {};
         }
-        selectedEmployee.fields[date.format('YYYY-MM-DD')] = value;
+
+        // Set "Day off" for weekends
+        if (date.isoWeekday() === 6 || date.isoWeekday() === 7) {
+          employee.fields[date.format('YYYY-MM-DD')] = 'Ditë e lirë' ;
+        } else if (employee.fields[date.format('YYYY-MM-DD')] !== value) {
+          employee.fields[date.format('YYYY-MM-DD')] = value;
+        }
+
         updatedCount++;
-  
-        // Update the Firestore database only for the selected employee
-        const employeeId = selectedEmployee.id;
-        db.collection('employees')
-          .doc(employeeId)
-          .set(
-            {
-              fields: {
-                [date.format('YYYY-MM-DD')]: selectedEmployee.fields[date.format('YYYY-MM-DD')],
-              },
-            },
-            { merge: true }
-          )
-          .catch((error) => {
-            alert('Error updating field value in Firebase:', error);
-          });
-      }
-  
+      });
+
       setEmployees(updatedEmployees);
-  
+
       if (updatedCount > 0) {
-        alert(`Field value updated for ${updatedCount} employee(s) successfully.`);
+        // alert(`Field value updated for ${updatedCount} employee(s) successfully.`);
       }
-    }
 
+      // Update the Firestore database for all employees
+      const batch = db.batch();
+      updatedEmployees.forEach((employee) => {
+        const employeeId = employee.id;
+        const employeeRef = db.collection('employees').doc(employeeId);
+        batch.set(
+          employeeRef,
+          {
+            fields: {
+              [date.format('YYYY-MM-DD')]: employee.fields[date.format('YYYY-MM-DD')],
+            },
+          },
+          { merge: true }
+        );
+      });
 
-    if (userType === 'employee' && selectedEmployee.email === useremail) {
-
-      const fieldExists = selectedEmployee.fields && selectedEmployee.fields[date.format('YYYY-MM-DD')];
-
-      if (fieldExists) {
-        alert('You have already updated this field. You cannot change your selection.');
-        return;
+      batch
+        .commit()
+        .then(() => {
+          alert('Field values updated in Firebase successfully.');
+        })
+        .catch((error) => {
+          alert('Error updating field value in Firebase:', error);
+        });
+    } else {
+      // Set the value for the selected employee's field
+      if (!selectedEmployee.fields) {
+        selectedEmployee.fields = {};
       }
-      selectedEmployee.fields[date.format('YYYY-MM-DD')] = value;
-      setEmployees(updatedEmployees);
-  
+
+      // Set "Day off" for weekends
+      if (date.isoWeekday() === 6 || date.isoWeekday() === 7) {
+        selectedEmployee.fields[date.format('YYYY-MM-DD')] = 'Ditë e lirë' ;
+      } else {
+        selectedEmployee.fields[date.format('YYYY-MM-DD')] = value;
+      }
+
+      updatedCount++;
+
+      // Update the Firestore database only for the selected employee
       const employeeId = selectedEmployee.id;
       db.collection('employees')
         .doc(employeeId)
         .set(
           {
             fields: {
-              [date.format('YYYY-MM-DD')]: value,
+              [date.format('YYYY-MM-DD')]: selectedEmployee.fields[date.format('YYYY-MM-DD')],
             },
           },
           { merge: true }
         )
-        .then(() => {
-          alert('Field value updated successfully.');
-        })
         .catch((error) => {
           alert('Error updating field value in Firebase:', error);
         });
     }
 
+    setEmployees(updatedEmployees);
 
-    
-    else {
-      // alert('Access denied: Only the employee can edit their column');
+    if (updatedCount > 0) {
+      alert(`Field value updated for ${updatedCount} employee(s) successfully.`);
     }
-  };
-  
-  
-  for (let day = 1; day <= daysInMonth; day++) {
-    const date = firstDayOfMonth.clone().add(day - 1, 'day');
-    const weekNumber = date.week();
-    const isWeekend = date.day() === 0 || date.day() === 6;
+  } else if (userType === 'employee' && selectedEmployee.email === useremail) {
+    const fieldExists = selectedEmployee.fields && selectedEmployee.fields[date.format('YYYY-MM-DD')];
 
+    if (fieldExists) {
+      alert('You have already updated this field. You cannot change your selection.');
+      return;
+    }
+
+    selectedEmployee.fields[date.format('YYYY-MM-DD')] = value;
+    setEmployees(updatedEmployees);
+
+    const employeeId = selectedEmployee.id;
+    db.collection('employees')
+      .doc(employeeId)
+      .set(
+        {
+          fields: {
+            [date.format('YYYY-MM-DD')]: value,
+          },
+        },
+        { merge: true }
+      )
+      .then(() => {
+        alert('Field value updated successfully.');
+      })
+      .catch((error) => {
+        alert('Error updating field value in Firebase:', error);
+      });
+  } else {
+    // alert('Access denied: Only the employee can edit their column');
+  }
+};
+
+
+const renderCalendarBody = () => {
+  const daysInMonth = currentDate.daysInMonth();
+  const firstDayOfMonth = currentDate.clone().startOf('month');
+  const today = moment();
+  const calendarRows = [];
+  const daysAlbanian = ['Hëne', 'Marte', 'Mërkure', 'Ejte', 'Premte', 'Shtune', 'Diele'];
+  const monthsAlbanian = {
+    January: 'Janar',
+    February: 'Shkurt',
+    March: 'Mars',
+    April: 'Prill',
+    May: 'Maj',
+    June: 'Qershor',
+    July: 'Korrik',
+    August: 'Gusht',
+    September: 'Shtator',
+    October: 'Tetor',
+    November: 'Nëntor',
+    December: 'Dhjetor'
+  };
+
+  for (let day = 1; day <= daysInMonth; day++) {
+    const date = firstDayOfMonth.clone();
+    const weekNumber = date.week();
+    const isWeekend = date.isoWeekday() === 6 || date.isoWeekday() === 7; // Check if the date is Saturday or Sunday
+
+    if (isWeekend) {
+      // Set "Day off" for weekends
+      employees.forEach((employee) => {
+        if (!employee.fields) {
+          employee.fields = {};
+        }
+        employee.fields[date.format('YYYY-MM-DD')] = 'Ditë e lirë' ;
+      });
+    }
     calendarRows.push(
       <tr key={day}>
-        <td>{date.format('MMMM')}</td>
-        <td>{weekNumber}</td>
-        <td>{date.format('DD MMMM YYYY')}</td>
-        <td>{date.format('dddd')}</td>
-        {employees.map((employee, index) => {
+ <td>{monthsAlbanian[date.format('MMMM')]}</td>
+         <td>{weekNumber}</td>
+         <td>{date.format('DD') + ' ' + monthsAlbanian[date.format('MMMM')] + ' ' + date.format('YYYY')}</td>
+        <td>{daysAlbanian[date.isoWeekday() - 1]}</td>
+                {employees.map((employee, index) => {
           const field = employee.fields[date.format('YYYY-MM-DD')] || '-';
-
           const color = getColorForField(field);
           const isUneditable = userType === 'employee' && employee.email !== useremail;
+
+          // Set "Day off" for weekends if the field is not already set
+          if (isWeekend && !field) {
+            employee.fields[date.format('YYYY-MM-DD')] = 'Ditë e lirë' ;
+          }
 
           return (
             <td
@@ -217,12 +283,12 @@ const CalendarTable = ({ userType,useremail }) => {
                 <CustomDropdown
                   options={[
                     '       -',
-                    'Day off',
-                    'Work day',
-                    'Work travel',
-                    'Vacation',
-                    'Special event',
-                    'Sick Leave',
+                    'Ditë e lirë' ,
+                    'Prezent në punë',
+                    'Udhëtim Zyrëtar',
+                    'Pushim Vjetor',
+                    'Event Special',
+                    'Pushim Mjekësor',
                   ]}
                   value={field}
                   backgroundColor={color}
@@ -234,26 +300,32 @@ const CalendarTable = ({ userType,useremail }) => {
         })}
       </tr>
     );
+
+    // Move to the next day
+    firstDayOfMonth.add(1, 'day');
   }
 
   return (
     <table className="calendar-table">
-    <thead >
-        <tr  >
-          <th  class="values">Month</th>
-          <th  class="values">Week</th>
-          <th class="values" >Date</th>
-          <th  class="values">Day</th>
-          {employees.map((employee) => (
-            <th key={employee.id}>{employee.name}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>{calendarRows}</tbody>
-    </table>
+    <thead className={`calendar-header ${isHeaderSticky ? 'sticky' : ''}`} ref={tableHeadRef}>
+      <tr>
+        <th className="qwee">Month</th>
+        <th className="qwee">Java</th>
+        <th className="qwee">Data</th>
+        <th className="qwee">Dita</th>
+        {employees.map((employee) => (
+          <th key={employee.id} className="qwee">
+            {employee.name}
+          </th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>{calendarRows}</tbody>
+  </table>
   );
 };
 
+  
   const handlePrevMonth = () => {
     setCurrentDate(currentDate.clone().subtract(1, 'month'));
   };
@@ -275,15 +347,15 @@ const CalendarTable = ({ userType,useremail }) => {
 
   const getColorForField = (field) => {
     switch (field) {
-      case 'Work travel':
+      case 'Udhëtim Zyrëtar':
         return '#6197e8';
-      case 'Vacation':
+      case 'Pushim Vjetor':
         return '#e87a61';
-      case 'Special event':
+      case 'Event Special':
         return 'lightgreen';
-        case 'Sick Leave':
+        case 'Pushim Mjekësor':
           return '#c230d9';
-        case 'Work day':
+        case 'Prezent në punë':
           return 'white';
       default:
         return 'lightgrey';
@@ -296,11 +368,29 @@ const CalendarTable = ({ userType,useremail }) => {
     navigate('/home/manageemployees');
   };
 
+  const monthsAlbania = {
+    January: 'Janar',
+    February: 'Shkurt',
+    March: 'Mars',
+    April: 'Prill',
+    May: 'Maj',
+    June: 'Qershor',
+    July: 'Korrik',
+    August: 'Gusht',
+    September: 'Shtator',
+    October: 'Tetor',
+    November: 'Nëntor',
+    December: 'Dhjetor'
+  };
+
+  const date = currentDate.clone();
+
+
   return (
     <div className="calendar-container">
-      <div className="calendar-header">
+      <div className="calendar-uperline">
         <button onClick={handlePrevMonth}>&#8249;</button>
-        <h2>{currentDate.format('MMMM YYYY')}</h2>
+        <h2>{monthsAlbania[date.format('MMMM')] +' '+currentDate.format('YYYY')}</h2>
         <button onClick={handleNextMonth}>&#8250;</button>
 
         {userType === 'hr' && (
